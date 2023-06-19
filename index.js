12a7e5ba7935b7360f8e76c538933c6d8b36dd34
Raj1128 <118893926+Raj1128@users.noreply.github.com>
2023-06-19 17:15:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -19,21 +19,30 @@ app.innerHTML = `
     <ul class="todos-list"></ul>
 </div>
 `;
+
+// state
+let todos = JSON.parse(localStorage.getItem('todos'))[];
+
 // selectors
 const root = document.querySelector('.todos');
 const list = root.querySelector('.todos-list');
+const count = root.querySelector('.todos-count');
+const clear = root.querySelector('.todos-clear');
 const form = document.forms.todos;
 const input = form.elements.todo;
 
 // functions
+function saveToStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
 
+}
 
 function renderTodos(todos) {
   let todoString = '';
   todos.forEach((todo, index) => {
     todoString += `
-   <li data-id="${index}">
-      <input type="checkbox">
+   <li data-id="${index}" ${todo.complete ? ' class="todos-complete"' : ''}>
+      <input type="checkbox"${todo.complete ? ' checked' : ''}>
       <span>${todo.label}</span>
       <button type="button"> </button>   
    </li>
@@ -41,6 +50,8 @@ function renderTodos(todos) {
   }
   );
   list.innerHTML = todoString;
+  count.innerText = todos.filter(todo => !todo.complete).length;
+  clear.computedStyleMap.display = todos.filter(todo => todo.complete).length ? 'block' : 'none';
 
 }
 function addTodo(event) {
@@ -55,16 +66,135 @@ function addTodo(event) {
     },
   ];
   renderTodos(todos);
+  saveToStorage(todos);
   input.value = '';
 }
+function updateTodo(event) {
+  const id = (parseInt event.target.parentNOde.getAttribute('data-id'), 10);
+  const complete = event.target.checked;
+  todos = todos.map((todo, index) => {
+    if (index === id) {
+      return {
+        ...todo,
+        complete
+      };
+    }
+    return todo;
+  });
+
+  renderTodos(todos);
+  saveToStorage(todos);
+}
+
+function editTodo(event) {
+  if (event.target.nodeName.toLowerCase() !== 'span') {
+    return;
+  }
+  const id = parseInt(event.target.parentNOde.getAttribute('data-id'), 10);
+  const todoLabel = todos[id].label;
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.value = todoLabel;
+
+  function handleEdit(event) {
+    event.stopPropagation();
+    const label = this.value;
+    if (this.value !== todoLabel) {
+      todos = todos.map((todo, index) => {
+        if (index === id) {
+          return {
+            ...todo,
+            label
+          }
+        }
+        return todo;
+      });
+      renderTodos(todos);
+      saveToStorage(todos);
+    }
+    // clean up
+    event.target.style.display = '';
+    this.removeEventListener('change', handleEdit);
+    this.remove();
+  }
+
+  event.target.style.display = 'none';
+  event.target.parentNOde.append(input);
+  input.addEventListener('change', handleEdit);
+  input.focus();
+}
+
+
+function deleteTodo(event) {
+  if (event.target.nodeName.toLowerCase() !== 'button') {
+    return;
+  }
+  const id = parseInt(event.target.parentNOde.getAttribute('data-id'), 10);
+  const label = event.target.previousElementSibling.innerText;
+  if (window.confirm(`Delete ${label}?`)) {
+    todos = todos.filter((todo, index) => index !== id);
+    renderTodos(todos);
+    saveToStorage(todos);
+  }
+}
+
+function clearCompleteTodos() {
+  const count = todos.filter(todo => todo.complete).length;
+  if (count === 0) {
+    return;
+  }
+  if (window.confirm(`Delete ${count} todos?`)) {
+    todos = todos.filter(todo => !todo.complete);
+    renderTodos(todos);
+    saveToStorage(todos);
+  }
+}
+
 
 
 // init
 function init() {
+  renderTodos();
   //   add Todo
   form.addEventListener('submit', addTodo);
+  // update todo
+  list.addEventListener('change', updateTodo)
+  // Edit Todo
+  list.addEventListener('dblclick', editTodo);
+  // Delete Todo
+  list.addEventListener('click', deleteTodo);
+  //  complete All Todos
+  clear.addEventListener('click', clearCompleteTodos);
 }
 
 init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
